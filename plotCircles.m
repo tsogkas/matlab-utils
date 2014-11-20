127147b24f8afd250dfc908be92c63a63db4896e
Stavros Tsogkas <statsogk@gmail.com>
2014-11-20 18:58:29 +0000

    added plotCircles.m

---
+++ plotCircles.m
@@ -0,0 +1,32 @@
+% PLOTCIRCLES  Plot circles on an image.
+%   
+%   PLOTCIRCLES(c,r)  plots a circle with center (c(1),c(2)) and radius r.
+%   If c is a Nx2 matrix and r a Nx1 vector, this function plots N circles,
+%   where the i-th circle has center (c(i,1),c(i,2)) and radius r(i).
+%       
+%   PLOTCIRCLES(c,r,Marker)  set color and marker used to plot the circles. 
+%   Marker can be one of the standard colors used in LineSpec for function
+%   plot (default: 'r').
+%
+%   PLOTCIRCLES(c,r,Color,LineWidth)  sets the width of the line used to
+%   plot the circles (default: 1).
+%
+%   PLOTCIRCLES(c,r,Color,LineWidth,MarkerSize) also sets the size of the 
+%   marker used (default: 1).
+%
+%   See also: cylinder, plot
+%
+%   Stavros Tsogkas, <stavros.tsogkas@ecp.fr>
+%   Last update: November 2014
+
+function plotCircles(c,r,Marker,LineWidth,MarkerSize)
+    if nargin < 5, MarkerSize = 1; end
+    if nargin < 4, LineWidth  = 1; end
+    if nargin < 3, Marker = 'r';   end
+    
+    [x,y,~] = cylinder(r);
+    for i=1:size(x,1)
+        plot(x(i,:)+c(i,1), y(i,:)+c(i,2), Marker,...
+            'LineWidth',LineWidth,'MarkerSize',MarkerSize); hold on;
+    end
+end

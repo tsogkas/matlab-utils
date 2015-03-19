cf5ebc148c39554133a35dd501d441331863f85a
Stavros Tsogkas <statsogk@gmail.com>
2015-03-19 15:53:16 +0100

    added softmax function, finished with make.m (documentation, flag parsing etc)

--- make.m
+++ make.m
@@ -1,34 +1,51 @@
-function make(target,debug)
-% MAKE Simulates make functionality and facilitates mex-file compilation.
+function make(target,flags)
+% MAKE Simulates GNU-make functionality and facilitates mex-file compilation.
+%   Edit make.m to set the source and build directories, include and link
+%   libraries and define the source files to be compiled into mex files.
 %
-%   MAKE('all') builds the default 
+%   MAKE all or MAKE('all') compiles all mex files.
+% 
+%   MAKE all -flags or MAKE('all',['-' flags]) allows user input to control
+%   debug, warnings, and verbose flags. flags can either be a single word
+%   or a sequence of flags.
+% 
+%   Examples:
+%   make all -debug or make('all','-debug'): compiles using debug symbols
+%   make all -warnings or make('all','-warnings'): prints all warnings
+%   make all -verbose or make('all','-verbose'): prints verbose information
+% 
+%   You can use a combination of flags in the following way:
+%   make all -dw:   compiles using debug symbols and prints all warnings
+%   make all -v:    be verbose
+%   make all -wvd:  set debug, warnings and verbose flags to true
+% 
+% 
+% Stavros Tsogkas, <stavros.tsogkas@centralesupelec.fr>
+% Last update: March 2015 
+
 
 narginchk(0,2);
 if nargin < 1, target = 'all'; end
 if nargin < 2
-    debug = false;
+    debug = false; verbose = false; warnings = false;
 else
-    debug = (islogical(debug) && debug) || ...
-             ismember(debug, {'debug','Debug','DEBUG','-d','-D'});
+    [debug,verbose,warnings] = parseFlags(flags);
 end
 
-% Flags
-verbose  = 0;
-warnings = 0;
-
 % Source files to be compiled and build directory
-sourceDir = 'src';  
-buildDir  = 'build'; 
+sourceDir   = 'src';      % your source files are here
+buildDir    = 'build';    % the mex file will be placed here
 sourceFiles = readSourceFiles(sourceDir);
-sourceFiles = ['examples/dense_inference_mex.cpp', 'examples/common.cpp',...
-    'examples/ppm.cpp', sourceFiles];
+
+% Source files to be compiled into mex files (add more at will)
+mexFiles    = {'examples/denseInferenceMex.cpp'}; 
 
 % Include directories and Link libraries
-INCLUDEDIRS   = {'include','external','examples','/usr/local/eigen',...
-    '/usr/local/lbfgs/include'};
-LINKLIBS      = {'/usr/local/lbfgs/lib -llbfgs'};
+INCLUDEDIRS = {'include','external','examples','include/Eigen3.2.4',...
+    'external/liblbfgs/include'};
+LINKLIBS    = {'external/liblbfgs/lib/ -llbfgs'};
 
-% Optimization and other flags
+% Optimization flags
 CXXOPTIMFLAGS = ' CXXOPTIMFLAGS="-O3 -DNDEBUG -fopenmp"';
 LDOPTIMFLAGS  = ' LDOPTIMFLAGS="-O3 -fopenmp"';
 CXXFLAGS      = ' CXXFLAGS="\$CXXFLAGS -Wall"';
@@ -51,13 +68,19 @@ switch target
         mexcmd = addLinkLibs(mexcmd, LINKLIBS);
         mexcmd = addSourceFiles(mexcmd, sourceFiles);
         disp(['Creating ' buildDir 'directory']); mkdir(buildDir);
-        disp(['Executing: ' mexcmd]); eval(mexcmd);
+        buildMexFiles(mexFiles, mexcmd);
     case 'clean'
-        deleteMexFiles(buildDir, sourceFiles)
+        deleteMexFiles(buildDir, mexFiles)
     otherwise
         error('Invalid make target')
 end
 
+function buildMexFiles(mexFiles, mexcmd)
+for i=1:numel(mexFiles)
+    [~,mexName] = fileparts(mexFiles{i}); 
+    mexcmdFull  = [mexcmd ' ' mexFiles{i} ' -output ' mexName];
+    disp(['Executing: ' mexcmdFull]); eval(mexcmdFull);
+end
 
 function mexcmd = addIncludeDirs(mexcmd, INCLUDEDIRS)
 for i=1:numel(INCLUDEDIRS)
@@ -78,7 +101,7 @@ end
 function deleteMexFiles(buildDir, sourceFiles)
 for i=1:numel(sourceFiles)
     [~,name] = fileparts(sourceFiles{i});
-    mexFile  = [name mexext];
+    mexFile  = [name '.' mexext];
     delete(fullfile(buildDir, mexFile));
 end
 
@@ -90,3 +113,14 @@ sourceFiles = {sourceFiles(:).name};
 for i=1:numel(sourceFiles)
     sourceFiles{i} = fullfile(sourceDir, sourceFiles{i});
 end
+
+function [debug,verbose,warnings] = parseFlags(flags)
+%TODO: better flag checking (assert that input flags are valid etc)
+assert(flags(1)=='-', 'Invalid syntax. Flags should be preceded by ''-''');
+flags    = flags(2:end);
+debug    = ismember(flags, {'debug','Debug','DEBUG'}) || ...
+    ~isempty(strfind(flags,'d')) || ~isempty(strfind(flags,'D'));
+verbose  = ismember(flags, {'verbose','Verbose','VERBOSE'}) || ...
+    ~isempty(strfind(flags,'v')) || ~isempty(strfind(flags,'V'));
+warnings = ismember(flags, {'warnings','Warnings','WARNINGS'}) || ...
+    ~isempty(strfind(flags,'w')) || ~isempty(strfind(flags,'W'));
---
+++ softmax.m
@@ -0,0 +1,25 @@
+function p = softmax(x)
+%SOFTMAX Softmax function that avoids overflow.
+% 
+%   P = SOFTMAX(X), X can be a vector, a matrix, or a MxNxK array. If X is
+%   a matrix, SOFTMAX calculates the softmax along the first dimension
+%   (matrix columns). If X is a MxNxK array, it calculates the softmax
+%   along the third dimension.
+% 
+% Stavros Tsogkas, <stavros.tsogkas@centralesupelec.fr>
+% Last update: March 2015 
+
+if isvector(x) 
+    expx = exp(x-max(x));
+    p = expx ./ sum(expx);
+elseif ismatrix(x)
+    expx = exp(bsxfun(@minus, x, max(x,[],1)));
+    p = bsxfun(@rdivide, expx, sum(expx,1));
+elseif size(x,3) > 1
+    expx = exp(bsxfun(@minus, x, max(x,[],3)));
+    p = bsxfun(@rdivide, expx, sum(expx,3));
+else
+    error('Input must be either a vector, a matrix, or a MxNxK array')
+end
+
+

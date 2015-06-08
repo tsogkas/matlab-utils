034fa6bd92b12baeb2039011962c938fbd66424e
Stavros Tsogkas <statsogk@gmail.com>
2015-06-08 21:21:43 +0200

    removed issymmetric.m

--- issymmetric.m
+++
@@ -1,8 +0,0 @@
-function res = issymmetric(A)
-% ISSYMMETRIC Check if input matrix is symmetric (returns true or false).
-% 
-% Stavros Tsogkas, <stavros.tsogkas@centralesupelec.fr>
-% Last update: March 2015 
-
-assert(ismatrix(A), 'A is not a matrix');
-res = isequal(A, A');

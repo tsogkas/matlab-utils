64da7045db1119baaea4fd49ab0f2ec44df94019
Stavros Tsogkas <statsogk@gmail.com>
2015-08-05 20:28:27 +0200

    added colormap choice as an input argument in montageArray

--- montageArray.m
+++ montageArray.m
@@ -1,17 +1,22 @@
+function montageArray(images,cmap)
 % MONTAGEARRAY  Simple wrapper for montage that facilitates function call
 % when the input is a MxNxK array
 %
-%   montageArray(images)  when images is a MxNxK array, first reshapes
-%   images to MxNx1xK and then calls MATLAB montage function with the jet
-%   colormap.
+%   MONTAGEARRAY(images)  reshapes images from a MxNxK to a MxNx1xK array
+%   and then calls MATLAB montage function.
 % 
+%   MONTAGEARRAY(images,cmap)  plots images using the desired colormap
+%   (cmap must be a valid colormap).
+%
 %   Stavros Tsogkas, <stavros.tsogkas@ecp.fr>
-%   Last update: November 2014
+%   Last update: August 2015
 
-
-function montageArray(images)
-   sz = size(images);
-   assert(length(sz)==3,'Input array is not 3D');
-   montage(reshape(images,sz(1),sz(2),1,sz(3)),jet)
+sz = size(images);
+assert(length(sz)==3,'Input array is not 3D');
+if nargin > 1
+    montage(reshape(images,sz(1),sz(2),1,sz(3)),cmap)
+else
+    montage(reshape(images,sz(1),sz(2),1,sz(3)))
 end
+
 

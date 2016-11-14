8092a13ce259105e70754ae92c39c726a9711cc9
Stavros Tsogkas <statsogk@gmail.com>
2016-11-14 16:04:23 -0500

    added size2string.m

---
+++ size2string.m
@@ -0,0 +1,39 @@
+function [str, sizeInBytes] = size2string(n,dtype)
+%  SIZE2STRING prints the size in RAM in human-readably format.
+% 
+%   s = size2string(n,dtype) where n is the number of elements and type is
+%   the data type ('double','single','int32', etc).
+% 
+%   Stavros Tsogkas <tsogkas@cs.toronto.edu>
+%   Last update: November 2016
+
+if nargin < 2, dtype = 'double'; end
+
+switch  dtype
+    case {'double','uint64','int64'}
+        sizeOfElementInBytes = 8;
+    case {'single','uint32','int32'}
+        sizeOfElementInBytes = 4;
+    case {'uint16','int16'}
+        sizeOfElementInBytes = 2;
+    case {'uint8','int8','char'}
+        sizeOfElementInBytes = 1;
+    case {'logical'}
+        sizeOfElementInBytes = 1/8;
+    otherwise error('Data type not supported')
+end
+
+sizeInBytes = sizeOfElementInBytes * n;
+
+KB = 1024;
+MB = 1024^2;
+GB = 1024^3;
+if sizeInBytes < KB 
+    str = [num2str(sizeInBytes) ' B'];
+elseif sizeInBytes < MB
+    str = [num2str(sizeInBytes/KB) ' KB'];
+elseif sizeInBytes < GB
+    str = [num2str(sizeInBytes/MB) ' MB'];
+else
+    str = [num2str(sizeInBytes/GB) ' GB'];
+end

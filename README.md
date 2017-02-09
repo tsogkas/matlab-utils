5e71d6c5932945d6e3f8a67d024872d2d530947c
Stavros Tsogkas <statsogk@gmail.com>
2017-02-08 20:28:41 -0500

    added TODO in README, udpated imresizeCrisp()

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 ### utils
 
 A collection of matlab utility functions.
+
+### TODO: 
+
+####### imresizeCrisp() 
+- Investigate what happens when the input is not binary (e.g. soft probability maps).
+
+####### progress() 
+- Find a way to make `progress` work within a `parfor` loop.
+
+####### New functions
+- Add functions for easier display of multiple side-by-side figures, update montageArray function.
+
+
+
+
--- imresizeCrisp.m
+++ imresizeCrisp.m
@@ -1,99 +1,110 @@
-function out = imresizeCrisp(in, scale, threshold, visualize)
+function out = imresizeCrisp(in, scale, compare)
     % IMRESIZECRISP Resize input image without blurring. 
-    %   IMRESIZECRISP works the same way as Matlab's imresize function but
-    %   without smearing and blurring its values because of interpolation
-    %   when downsampling.Pixel coordinates in the input image are mapped 
-    %   to coordinates in the output image. The value of a pixel in the 
-    %   output image is the maximum of the values of all points in the 
-    %   original image that are mapped to the same point in the output. 
+    %   IMRESIZECRISP works in a similar way as Matlab's imresize function
+    %   with the 'nearest' method, but without "erasing" pixels when
+    %   downsampling a very sparse input, such as boundary maps. Pixel 
+    %   coordinates in the input space are mapped to coordinates in the
+    %   output image. The value of a pixel in the output image is the 
+    %   maximum of the values of all points in the original image that are
+    %   mapped to it. 
     %   
-    %   This function can be particularly useful when we want to resize
-    %   thin structures, like boundary maps.
-    %   IMRESIZECRISP only works for grayscale images at the moment when
-    %   downsampling.
-    %
     %   out = IMRESIZECRISP(in,scale) where scale is a scalar resizes an image
     %   so that size(output) = scale*size(input). If scale > 1, then
-    %   IMRESIZECRISP calls MATLAB's imresize function to upsample the image.
-    %   If scale < 1, the image is downsampled.
+    %   IMRESIZECRISP calls MATLAB's imresize function with the 'nearest'
+    %   method to upsample the image. If scale < 1, the image is downsampled.
     %
-    %   out = IMRESIZECRISP(in, [h,w]), resizes the input image in, so that
-    %   to dimensions [h,w].
+    %   out = IMRESIZECRISP(in, [H,W]), resizes the input image so that the
+    %   output is HxW.
+    %
+    %   NOTE: This function only works for inputs with non-negative values.
+    %
+    %   When the input is a HxWxC array, IMRESIZECRISP is applied to all
+    %   channels.
     %
     % Stavros Tsogkas, <tsogkas@cs.toronto.edu>
-    % Last update: January 2017
+    % Last update: February 2017
 
     
-    if nargin < 3, threshold = 0.1; end
-    if nargin < 4, visualize = false; end
-    
+    % Parse arguments and compute output dimensions
+    if nargin < 3, compare = false; end
     if isscalar(scale)  % scaling factor
         if scale == 1   
             out = in;
             return
         elseif scale > 1
-            out = imresize(in,scale,'bilinear');
+            out = imresize(in,scale,'nearest');
             return
+        else
+            [Hin,Win,Cin] = size(in);
+            scaleHor = scale;
+            scaleVer = scale;
+            Hout     = round(Hin*scaleVer);
+            Wout     = round(Win*scaleHor);
+            Cout     = Cin;
         end
-        [h,w,~]  = size(in);
-        scaleHor = scale;
-        scaleVer = scale;
-        hnew     = round(h*scaleVer);
-        wnew     = round(w*scaleHor);
-    elseif numel(scale) == 2
-        if scale(1) > size(in,1)
-            in = imresize(in,[scale(1), size(in,2)],'bilinear');
+    elseif numel(scale) == 2 % resize to [H,W]
+        if scale(1) == size(in,1) && scale(2) == size(in,2)
+            out = in;
+            return
+        elseif scale(1) > size(in,1) && scale(2) > size(in,2)
+            out = imresize(in,scale,'nearest');
+            return
+        else
+            % We use two separate resizing steps because we may have 
+            % upsampling in one dimension and downsampling in the other.
+            if scale(1) > size(in,1)
+                in = imresize(in,[scale(1), size(in,2)],'nearest');
+            end
+            if scale(2) > size(in,2)
+                in = imresize(in,[size(in,1), scale(2)],'nearest');
+            end
+            [Hin,Win,Cin] = size(in);
+            Hout     = scale(1);
+            Wout     = scale(2);
+            Cout     = Cin;
+            scaleHor = Wout / Win;
+            scaleVer = Hout / Hin;
         end
-        if scale(2) > size(in,2)
-            in = imresize(in,[size(in,1), scale(2)],'bilinear');
-        end
-        [h,w,~]  = size(in);
-        hnew     = scale(1);
-        wnew     = scale(2);        
-        scaleHor = wnew / w;
-        scaleVer = hnew / h;
     else
         error('scale must be a scalar or a [height, width] vector')
     end
     
+    % Get coordinates of nonzero and nonnegative 
+    inds = find(in > 0);
+    vals = in(inds);
     
-    
-    % Original coordinates of thresholded image
-    inThresh = in; inThresh(inThresh <= threshold) = 0;
-    inMax = inThresh;
-    [y,x] = find(inMax > threshold);
-    inds  = sub2ind([h,w],y,x);
-    vals  = inMax(inds);
-    
-    
-    % Map to new coordinates
-    ynew = ceil(y*scaleVer);
-    xnew = ceil(x*scaleHor);
-    offLimits = xnew > wnew | ynew > hnew | xnew < 1 | ynew < 1;
-    xnew(offLimits) = [];   % remove indices that fall outside of the image
-    ynew(offLimits) = [];
+    % Map to new coordinates. In the case of downsampling, multiple input
+    % coordinates may be mapped to the same output coordinates.
+    [yout,xout,zout]= ind2sub([Hin,Win],inds);
+    yout = ceil(yout*scaleVer);
+    xout = ceil(xout*scaleHor);
+    % Remove indices that fall outside of the image
+    offLimits = xout > Wout | yout > Hout | xout < 1 | yout < 1;
+    xout(offLimits) = [];   
+    yout(offLimits) = [];
+    zout(offLimits) = [];
     vals(offLimits) = [];
     
-    % We sort all values first to automatically obtain the max value
-    % for each mapped point later: 'unique' returns indexes IA which point 
-    % to the first occurence of each element. If vals is sorted, that index
-    % corresponds to the maximum value for that pixel by default.
+    % We sort all values first to later obtain more easily the max among
+    % possibly multiple values corresponding to the same output point. 
     [vals, indSort] = sort(vals,'descend');
-    xnew = xnew(indSort); ynew = ynew(indSort);
-    indsNew = sub2ind([hnew,wnew],ynew,xnew);
+    xout = xout(indSort); yout = yout(indSort);
     
-    % Fill in values in resized output
-    out = zeros(hnew,wnew);
-    [uniqueInds,IA] = unique(indsNew);
+    % unique() returns indexes IA which point to the first occurence of
+    % each element. If vals is sorted, that index corresponds to the 
+    % maximum value for that pixel by default.
+    [uniqueInds,IA] = unique(sub2ind([Hout,Wout,Cout],yout,xout,zout));
+
+    % Fill output values
+    out = zeros(Hout,Wout,Cout);
     out(uniqueInds) = vals(IA);
     
-    % Visualize results
-    if visualize
+    % Compare results
+    if compare
         figure;
-        subplot(221); imshow(in); title('original');
-        subplot(222); imshow(inThresh); title('thresholded'); 
-        subplot(223); imshow(inMax); title('maximum');
-        subplot(224); imshow(out); title('output'); 
+        subplot(131); imshow(in); title('Input');
+        subplot(132); imshow(imresize(in,scale,'nearest')); title('Nearest-neighbor'); 
+        subplot(133); imshow(out); title('Crisp');
     end
 end
 

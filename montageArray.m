19cc6ac71d2d0742540cbaba6ede663b66d7a71e
Stavros Tsogkas <statsogk@gmail.com>
2017-02-12 00:44:35 -0500

    montageArray() now can take the same arguments as Matlab's montage()

--- montageArray.m
+++ montageArray.m
@@ -1,6 +1,6 @@
-function montageArray(images,cmap)
+function montageArray(images,varargin)
 % MONTAGEARRAY  Simple wrapper for montage that facilitates function call
-% when the input is a MxNxK array
+%   when the input is a MxNxK array.
 %
 %   MONTAGEARRAY(images)  reshapes images from a MxNxK to a MxNx1xK array
 %   and then calls MATLAB montage function.
@@ -8,9 +8,10 @@ function montageArray(images,cmap)
 %   MONTAGEARRAY(images,cmap)  plots images using the desired colormap
 %   (cmap must be a valid colormap).
 %
-%   Stavros Tsogkas, <stavros.tsogkas@centralesupelec.fr>
-%   Last update: April 2016
+%   Stavros Tsogkas, <tsogkas@cs.toronto.edu>
+%   Last update: February 2017
 
+% Reshape MxNxK input to a MxNx1xK or a MxNx3xK array
 sz = size(images);
 if ndims(images) == 3
     images = reshape(images, sz(1),sz(2),1,sz(3));
@@ -19,8 +20,6 @@ elseif ndims(images) > 4
 end
 assert(size(images,3) == 1 || size(images,3) == 3, ...
     'Images should be either grayscale or truecolor')
-if nargin > 1
-    montage(images, cmap)
-else
-    montage(images)
-end
+
+% Call montage() function to do the rest
+montage(images, varargin{:})
